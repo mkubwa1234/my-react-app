0539eb2cc74edd61f025f913a319460daafcd6bc
Alpha <alpha.matara@student.moringaschool.com>
2024-05-09 10:33:25 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Card from './Components/BotCard';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/MyBotArmy/:id",
+    element: <Card />,
+  }
+ ])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
